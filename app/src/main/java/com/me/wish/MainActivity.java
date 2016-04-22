373a6d4aa67c84800e5b77d7f41cee8c055cfd9d
EricaHe <esis_1025@126.com>
2016-04-22 15:32:52 +0800

    change to expandable list view

--- app/src/main/java/com/me/wish/MainActivity.java
+++ app/src/main/java/com/me/wish/MainActivity.java
@@ -1,8 +1,11 @@
 package com.me.wish;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,15 +15,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final String[] wishes = new String[] {
-        "first", "second", "third", "fourth", "fifth"
-    };
+    private List<String> wishes;
+    private List<List<String>> childWishes;
+
+    protected void dataInitialization(){
+        wishes = new ArrayList<String>();
+        childWishes = new ArrayList<List<String>>();
+        wishes.add("Lose some weight");
+        wishes.add("Learning English");
+
+        List<String> tempArray = new ArrayList<String>();
+        tempArray.add("Jogging 2 km");
+        tempArray.add("Have more vegetable for dinner");
+
+        childWishes.add(tempArray);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +52,35 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListView wishListView = (ListView) findViewById(R.id.wishListView);
-        wishListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, wishes));
+        dataInitialization();
+
+        ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        expandableListView.setGroupIndicator(null);
+        // 监听组点击
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener()
+        {
+            @SuppressLint("NewApi")
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id)
+            {
+                return childWishes.get(groupPosition).isEmpty();
+            }
+        });
+
+        // 监听每个分组里子控件的点击事件
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener()
+        {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id)
+            {
+                Toast.makeText(MainActivity.this, "group=" + groupPosition + "---child=" + childPosition + "---" +
+                        childWishes.get(groupPosition).get(childPosition), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        MyExpandableListViewAdapter adapter = new MyExpandableListViewAdapter(this);
+        expandableListView.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +99,218 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    // 用过ListView的人一定很熟悉，只不过这里是BaseExpandableListAdapter
+    // TODO:子菜单为空会强退，要对子心愿长度做检查
+    // TODO:界面排版
+    class MyExpandableListViewAdapter extends BaseExpandableListAdapter
+    {
+        private Context context;
+        public MyExpandableListViewAdapter(Context context)
+        {
+            this.context = context;
+        }
+
+        /**
+         *
+         * 获取组的个数
+         *
+         * @return
+         * @see android.widget.ExpandableListAdapter#getGroupCount()
+         */
+        @Override
+        public int getGroupCount()
+        {
+            return wishes.size();
+        }
+
+        /**
+         *
+         * 获取指定组中的子元素个数
+         *
+         * @param groupPosition
+         * @return
+         * @see android.widget.ExpandableListAdapter#getChildrenCount(int)
+         */
+        @Override
+        public int getChildrenCount(int groupPosition)
+        {
+            return childWishes.get(groupPosition).size();
+        }
+
+        /**
+         *
+         * 获取指定组中的数据
+         *
+         * @param groupPosition
+         * @return
+         * @see android.widget.ExpandableListAdapter#getGroup(int)
+         */
+        @Override
+        public Object getGroup(int groupPosition)
+        {
+            return wishes.get(groupPosition);
+        }
+
+        /**
+         *
+         * 获取指定组中的指定子元素数据。
+         *
+         * @param groupPosition
+         * @param childPosition
+         * @return
+         * @see android.widget.ExpandableListAdapter#getChild(int, int)
+         */
+        @Override
+        public Object getChild(int groupPosition, int childPosition)
+        {
+            return childWishes.get(groupPosition).get(childPosition);
+        }
+
+        /**
+         *
+         * 获取指定组的ID，这个组ID必须是唯一的
+         *
+         * @param groupPosition
+         * @return
+         * @see android.widget.ExpandableListAdapter#getGroupId(int)
+         */
+        @Override
+        public long getGroupId(int groupPosition)
+        {
+            return groupPosition;
+        }
+
+        /**
+         *
+         * 获取指定组中的指定子元素ID
+         *
+         * @param groupPosition
+         * @param childPosition
+         * @return
+         * @see android.widget.ExpandableListAdapter#getChildId(int, int)
+         */
+        @Override
+        public long getChildId(int groupPosition, int childPosition)
+        {
+            return childPosition;
+        }
+
+        /**
+         *
+         * 组和子元素是否持有稳定的ID,也就是底层数据的改变不会影响到它们。
+         *
+         * @return
+         * @see android.widget.ExpandableListAdapter#hasStableIds()
+         */
+        @Override
+        public boolean hasStableIds()
+        {
+            return true;
+        }
+
+        /**
+         *
+         * 获取显示指定组的视图对象
+         *
+         * @param groupPosition 组位置
+         * @param isExpanded 该组是展开状态还是伸缩状态
+         * @param convertView 重用已有的视图对象
+         * @param parent 返回的视图对象始终依附于的视图组
+         * @return
+         * @see android.widget.ExpandableListAdapter#getGroupView(int, boolean, android.view.View,
+         *      android.view.ViewGroup)
+         */
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent)
+        {
+            GroupHolder groupHolder = null;
+            if (convertView == null)
+            {
+                convertView = LayoutInflater.from(context).inflate(R.layout.wish_expand_list, null);
+                groupHolder = new GroupHolder();
+                groupHolder.txt = (TextView)convertView.findViewById(R.id.wish_item);
+                groupHolder.img = (ImageView)convertView.findViewById(R.id.wish_item_caret);
+                convertView.setTag(groupHolder);
+            }
+            else
+            {
+                groupHolder = (GroupHolder)convertView.getTag();
+            }
+
+            if (!isExpanded)
+            {
+                groupHolder.img.setBackgroundResource(android.R.drawable.arrow_down_float);
+            }
+            else
+            {
+                groupHolder.img.setBackgroundResource(android.R.drawable.arrow_up_float);
+            }
+
+            groupHolder.txt.setText(wishes.get(groupPosition));
+            return convertView;
+        }
+
+        /**
+         *
+         * 获取一个视图对象，显示指定组中的指定子元素数据。
+         *
+         * @param groupPosition 组位置
+         * @param childPosition 子元素位置
+         * @param isLastChild 子元素是否处于组中的最后一个
+         * @param convertView 重用已有的视图(View)对象
+         * @param parent 返回的视图(View)对象始终依附于的视图组
+         * @return
+         * @see android.widget.ExpandableListAdapter#getChildView(int, int, boolean, android.view.View,
+         *      android.view.ViewGroup)
+         */
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent)
+        {
+            ItemHolder itemHolder = null;
+            if (convertView == null)
+            {
+                convertView = LayoutInflater.from(context).inflate(R.layout.child_wish_expand_list, null);
+                itemHolder = new ItemHolder();
+                itemHolder.txt = (TextView)convertView.findViewById(R.id.child_wish_item);
+                convertView.setTag(itemHolder);
+            }
+            else
+            {
+                itemHolder = (ItemHolder)convertView.getTag();
+            }
+            itemHolder.txt.setText(childWishes.get(groupPosition).get(childPosition));
+            return convertView;
+        }
+
+        /**
+         *
+         * 是否选中指定位置上的子元素。
+         *
+         * @param groupPosition
+         * @param childPosition
+         * @return
+         * @see android.widget.ExpandableListAdapter#isChildSelectable(int, int)
+         */
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition)
+        {
+            return true;
+        }
+
+    }
+
+    class GroupHolder
+    {
+        public TextView txt;
+        public ImageView img;
+    }
+
+    class ItemHolder
+    {
+        public ImageView img;
+        public TextView txt;
     }
 
     @Override
