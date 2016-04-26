08bb38c3a65fc52f51482593768067b64af4b108
EricaHe <esis_1025@126.com>
2016-04-26 12:42:26 +0800

    the layout of add activity

--- app/src/main/java/com/me/wish/AddActivity.java
+++ app/src/main/java/com/me/wish/AddActivity.java
@@ -32,7 +32,6 @@ public class AddActivity extends AppCompatActivity {
         childWishListView = (ListView)findViewById(R.id.addChildWishListView);
         childWishListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked,
                 childWish));
-        fixListViewHeight(childWishListView);
 
         addChildWishImgBtn = (ImageButton)findViewById(R.id.addChildWishImgBtn);
         childWishEditTxt = (EditText)findViewById(R.id.childWishEditText);
@@ -46,7 +45,6 @@ public class AddActivity extends AppCompatActivity {
                     childWish.add(extraWish);
                     childWishListView.setAdapter(new ArrayAdapter<String>(AddActivity.this,
                             android.R.layout.simple_list_item_checked, childWish));
-                    fixListViewHeight(childWishListView);
                     childWishEditTxt.setText("");
                 }
             }
@@ -75,25 +73,4 @@ public class AddActivity extends AppCompatActivity {
     }
     private List<String> childWish = new ArrayList<String>();
 
-    public void fixListViewHeight(ListView listView) {
-        // 如果没有设置数据适配器，则ListView没有子项，返回。
-        ListAdapter listAdapter = listView.getAdapter();
-        int totalHeight = 0;
-        if (listAdapter == null) {
-            return;
-        }
-        for (int index = 0, len = listAdapter.getCount(); index < len; index++) {
-            View listViewItem = listAdapter.getView(index , null, listView);
-            // 计算子项View 的宽高
-            listViewItem.measure(0, 0);
-            // 计算所有子项的高度和
-            totalHeight += listViewItem.getMeasuredHeight();
-        }
-
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        // listView.getDividerHeight()获取子项间分隔符的高度
-        // params.height设置ListView完全显示需要的高度
-        params.height = totalHeight+ (listView.getDividerHeight() * (listAdapter.getCount() - 1));
-        listView.setLayoutParams(params);
-    }
 }
